4cc7be6a2df5828d7cf6de12dce01d5521293c99
Orlando <orlando.osberto@gmail.com>
2022-05-10 13:44:53 -0600

    Colocando que el servidor acepte multiples conexiones usando pthreads
    Tambien le coloque al makefile la flag para que pueda correr pthreads

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ clean:
 	rm server_exe
 
 client_exe: client.cpp
-	g++ -o client_exe client.cpp register.pb.cc -lprotobuf
+	g++ -o client_exe client.cpp register.pb.cc -lprotobuf -lpthread 
 	
 server_exe: server.cpp
-	g++ -o server_exe server.cpp register.pb.cc -lprotobuf
+	g++ -o server_exe server.cpp register.pb.cc -lprotobuf --std=c++0x -lpthread 
--- server.cpp
+++ server.cpp
@@ -18,24 +18,22 @@ Juan Pablo Pineda */
 
 using namespace std;
 using namespace google::protobuf;
-
-int main()
-{
-
-    struct user
+struct userInformation
     {
         string name;
         int socket;
         string ip;
         string status;
     };
+pthread_t tid;
 
+int init () {
      // Create a socket
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1)
     {
         cerr << "Can't create a socket! Quitting" << endl;
-        return -1;
+        return 0;
     }
  
     // Bind the ip address and port to a socket
@@ -48,20 +46,18 @@ int main()
  
     // Tell Winsock the socket is for listening
     listen(listening, SOMAXCONN);
- 
-    // Wait for a connection
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
- 
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
- 
+    return listening;
+}
+
+void* clientConnection (void *args){
+    int clientSocket = (int) args;
     char host[NI_MAXHOST];      // Client's remote name
     char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
  
     memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
     memset(service, 0, NI_MAXSERV);
  
-    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+    /* if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
     {
         cout << host << " connected on port " << service << endl;
     }
@@ -69,14 +65,13 @@ int main()
     {
         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
         cout << host << " connected on port " << ntohs(client.sin_port) << endl;
-    }
+    } */
  
     // Close listening socket
-    close(listening);
+    // close(listening);
  
     // While loop: accept and echo message back to client
     char buf[4096];
- 
     while (true)
     {
         memset(buf, 0, 4096);
@@ -88,14 +83,20 @@ int main()
             cerr << "Error in recv(). Quitting" << endl;
             break;
         }
- 
+
         if (bytesReceived == 0)
         {
             cout << "Client disconnected " << endl;
             break;
         }
- 
-        cout << string(buf, 0, bytesReceived) << endl;
+        // TODO cambiar esto para que solo se lo cierre del lado del cliente y hacer uno para cerrar el servidor
+        if (string(buf, 0, bytesReceived) == "exit"){
+            send(clientSocket, "Terminando conexion...", bytesReceived + 1, 0);
+            close(clientSocket);
+        }
+
+        // TODO cambiar esto para que muestre el nombre de cada usuario
+        cout << host<< "> " <<string(buf, 0, bytesReceived) << endl;     //Aqui se muestra el mensaje que mando el usuario
  
         // Echo message back to client
         send(clientSocket, buf, bytesReceived + 1, 0);
@@ -103,6 +104,62 @@ int main()
  
     // Close the socket
     close(clientSocket);
+}   
+
+int main()
+{
+    
+    // Wait for a connection
+    int listening = init();
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    string userInput;
+    int number = 2;
+    bool run = true;
+    int rc;
+    //while (run){
+    //    cout << "Haciendo pruebas" << endl;
+    //    getline(cin, userInput);
+    //    if (userInput == "exit"){
+    //        return 0;
+    //    }
+    //    rc = pthread_create(&tid, NULL, clientConnection,nullptr);
+    //    if(rc == 0)			/* could not create thread */
+    //    {
+    //        printf("\n ERROR: return code from pthread_create is %d \n", rc);
+    //        exit(1);
+    //    }  
+    //}
+    while (run){
+        int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+        pthread_create(&tid, NULL, clientConnection,(void *)clientSocket);
+        cout << "Se ha conectado un nuevo usuario" << endl;
+    }
  
     return 0;
 }
+
+
+
+
+// Cosas que me serviran mas adelante
+/* std::vector<userInformation> userList;
+    userInformation user;
+    chat::ServerResponse response;
+    chat::ServerResponse *response2 = new chat::ServerResponse();
+    chat::UserRegistration *reg= new chat::UserRegistration();;
+    chat::ClientRequest request;
+    user.name="Orlando";
+    user.socket=123;
+    user.ip="123";
+    user.status="on";
+    reg -> set_username("Orlando");
+    reg -> set_ip("123");
+    request.set_allocated_newuser(reg);   
+    //user.name = "Orlando";
+    //user.socket = 123;
+    //user.ip = "195";
+    //user.status = "on";
+    //userList.push_back(user);
+    //cout << userInformation.username() << endl;
+    //cout << request.has_newuser() << endl; */
